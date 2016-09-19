08b01ec5dd85a6f69e8a1ec21dd84ee748a1cebd
Jagadeesh Meesala <jagadeesh.hya@gmail.com>
2016-09-19 14:45:10 -0400

    removed unused variable

--- topic5_filtered_ls.js
+++ topic5_filtered_ls.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 var path = require('path');
-var matchedFiles = [];
 if(process.argv[2] != null && process.argv[3] != null{
  fs.readdir(process.argv[2],function(err, files){
   if(err === null){
